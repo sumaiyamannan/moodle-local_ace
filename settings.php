ed104673015e4172aeb3eb1ef4441dc9fcbece58
Dan Marsden <dan@danmarsden.com>
2021-10-21 12:42:29 +1300

    Remove Task clean up from local_ace plugin - has been moved into another location.

--- settings.php
+++ settings.php
@@ -31,26 +31,6 @@ if ($hassiteconfig) {
 
     if ($ADMIN->fulltree) {
 
-        $options = array(
-            0 => new lang_string('neverdeletelogs'),
-            1000 => new lang_string('numdays', '', 1000),
-            365 => new lang_string('numdays', '', 365),
-            180 => new lang_string('numdays', '', 180),
-            150 => new lang_string('numdays', '', 150),
-            120 => new lang_string('numdays', '', 120),
-            90 => new lang_string('numdays', '', 90),
-            60 => new lang_string('numdays', '', 60),
-            35 => new lang_string('numdays', '', 35),
-            10 => new lang_string('numdays', '', 10),
-            5 => new lang_string('numdays', '', 5),
-            2 => new lang_string('numdays', '', 2),
-            1 => new lang_string('numdays', '', 1));
-
-        $settings->add(new admin_setting_configselect(
-            'local_ace/allloglifetime',
-            new lang_string('allloglifetime', 'local_ace'),
-            new lang_string('configallloglifetime', 'local_ace'), 90, $options));
-
         $settings->add(new admin_setting_configtext(
             'local_ace/displayperiod',
             new lang_string('displayperiod', 'local_ace'),
