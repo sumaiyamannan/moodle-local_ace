fa232e90b29364b0940fc45a4ef3f0b27159bfd8
Dan Marsden <dan@danmarsden.com>
2021-12-07 20:10:59 +1300

    bump version

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_ace';
-$plugin->release = 2021120700;
-$plugin->version = 2021120700;
+$plugin->release = 2021120701;
+$plugin->version = 2021120701;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_STABLE;
 
